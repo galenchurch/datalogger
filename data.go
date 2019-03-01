f25ea859aa4cd86ef4e8722e21d054590847e96e
Galen Church <galenechurch@gmail.com>
2019-03-01 14:04:53 -0500

    data

---
+++ data.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"periph.io/x/periph/conn/physic"
+	"periph.io/x/periph/conn/spi"
+	"periph.io/x/periph/conn/spi/spireg"
+	"periph.io/x/periph/host"
+)
+
+func main() {
+	fmt.Printf("test")
+	log.Print("Test")
+	spitest()
+}
+
+func spitest() {
+	// Make sure periph is initialized.
+	if _, err := host.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Use spireg SPI port registry to find the first available SPI bus.
+	p, err := spireg.Open("")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer p.Close()
+
+	// Convert the spi.Port into a spi.Conn so it can be used for communication.
+	c, err := p.Connect(physic.MegaHertz, spi.Mode1, 8)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Write 0x10 to the device, and read a byte right after.
+	write := []byte{0x00, 0x00}
+	read := make([]byte, len(write))
+	if err := c.Tx(write, read); err != nil {
+		log.Fatal(err)
+	}
+	// Use read.
+	fmt.Printf("data: %v\n", read[1:])
+}
