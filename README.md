937bba4e19abc6c27f7cb09132ff399c43866694
Galen Church <galenechurch@gmail.com>
2019-03-01 17:29:42 -0500

    moving averge

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 build for BBB (ARM)
 
     env GOOS=linux GOARCH=arm GOARM=7 go build
+
+Enable SPI
+
+    echo BB-SPIDEV0 | sudo tee /sys/devices/bone_capemgr.*/slots
--- data.go
+++ data.go
@@ -8,12 +8,31 @@ import (
 	"periph.io/x/periph/conn/spi"
 	"periph.io/x/periph/conn/spi/spireg"
 	"periph.io/x/periph/host"
+
+	movingaverage "github.com/RobinUS2/golang-moving-average"
 )
 
 func main() {
 	fmt.Printf("test")
 	log.Print("Test")
 	spitest()
+}
+
+type dLog struct {
+	ma *movingaverage.MovingAverage
+}
+
+func parseMeasurement(r []byte) float64 {
+
+	MSB := r[0] & 0x1F
+	MSB = MSB << 8
+	val := MSB + r[1]
+	return (float64(val) - 4096) / 80
+}
+
+func (d dLog) moving(a float64) float64 {
+	d.ma.Add(a)
+	return d.ma.Avg()
 }
 
 func spitest() {
@@ -35,12 +54,24 @@ func spitest() {
 		log.Fatal(err)
 	}
 
-	// Write 0x10 to the device, and read a byte right after.
-	write := []byte{0x00, 0x00}
-	read := make([]byte, len(write))
-	if err := c.Tx(write, read); err != nil {
-		log.Fatal(err)
+	logger := dLog{ma: movingaverage.New(10)}
+
+	for index := 0; index < 15; index++ {
+		// Write 0x10 to the device, and read a byte right after.
+		write := []byte{0x00, 0x00}
+		read := make([]byte, len(write))
+		if err := c.Tx(write, read); err != nil {
+			log.Fatal(err)
+		}
+		// Use read.
+		fmt.Printf("%v\n", read)
+
+		meas := parseMeasurement(read)
+
+		ave := logger.moving(meas)
+
+		fmt.Printf("Measurement: %v\n", meas)
+		fmt.Printf("Moving average: %v\n", ave)
 	}
-	// Use read.
-	fmt.Printf("%v\n", read)
+
 }
